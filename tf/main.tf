bab6fdb6e6733042c39aca54ded789af30cad621
abhirk135 <abhirk135@gmail.com>
2025-08-19 19:26:26 -0400

    update

--- tf/main.tf
+++ tf/main.tf
@@ -5,3 +5,24 @@ resource "aws_ecr_repository" "app_repo" {
     scan_on_push = true
   }
 }
+resource "aws_ecr_lifecycle_policy" "app_lifecycle_policy" {
+  repository = aws_ecr_repository.app_repo.name
+  policy     = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images older than 30 days"
+        selection    = {
+          tagStatus = "untagged"
+          countType = "sinceImagePushed"
+          countUnit = "days"
+          countNumber = 30
+        }
+        action      = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
