aac311a624197d87ca0e65433412d5842024eec6
abhirk135 <abhirk135@gmail.com>
2025-08-19 23:29:58 -0400

    refine configuation

--- tf/local.tf
+++ tf/local.tf
@@ -2,8 +2,8 @@ locals {
   vpc_name              = "ark-vpc"
   subnet_name           = "ark-subnet"
   vpc_cidr_block        = "10.0.0.0/22"
-  public_a_subnet_cidr  = "10.0.1.0/24"
-  public_b_subnet_cidr  = "10.0.4.0/24"
+  public_a_subnet_cidr  = "10.0.0.0/24"
+  public_b_subnet_cidr  = "10.0.1.0/24"
   private_a_subnet_cidr = "10.0.2.0/24"
   private_b_subnet_cidr = "10.0.3.0/24"
 
