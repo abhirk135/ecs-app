eff8c7e5c9686996934cd4359bb052a90db40be1
abhirk135 <abhirk135@gmail.com>
2025-08-20 09:33:18 -0400

    refine configuation

--- tf/main.tf
+++ tf/main.tf
@@ -1,10 +1,11 @@
 resource "aws_ecr_repository" "ark_repo" {
-  name = var.ecr_repo_name
+  name                 = var.ecr_repo_name
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
 }
+
 resource "aws_ecr_lifecycle_policy" "ark_lifecycle_policy" {
   repository = aws_ecr_repository.ark_repo.name
   policy     = jsonencode({
@@ -13,12 +14,12 @@ resource "aws_ecr_lifecycle_policy" "ark_lifecycle_policy" {
         rulePriority = 1
         description  = "Expire untagged images older than 30 days"
         selection    = {
-          tagStatus = "untagged"
-          countType = "sinceImagePushed"
-          countUnit = "days"
-          countNumber = 30
+          tagStatus    = "untagged"
+          countType    = "sinceImagePushed"
+          countUnit    = "days"
+          countNumber  = 30
         }
-        action      = {
+        action = {
           type = "expire"
         }
       }
@@ -28,7 +29,7 @@ resource "aws_ecr_lifecycle_policy" "ark_lifecycle_policy" {
 
 #### Permissions setup for ECS ####################################
 resource "aws_iam_role" "ecs_task_execution" {
-  name = "arkEcsTaskExecutionRole"
+  name               = "${var.cluster_name}-task-execution-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
 }
 
@@ -37,10 +38,14 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-##### Task definition and Service #####################
+##### ECS Cluster ###############################################
+resource "aws_ecs_cluster" "ark_cluster" {
+  name = var.cluster_name
+}
 
+##### ECS Task Definition #######################################
 resource "aws_ecs_task_definition" "ark_task" {
-  family                   = "ark"
+  family                   = var.container_name
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
@@ -49,14 +54,12 @@ resource "aws_ecs_task_definition" "ark_task" {
 
   container_definitions = jsonencode([
     {
-      name      = "ark"
-      image     = "${aws_ecr_repository.ark_repo.repository_url}:latest"
+      name      = var.container_name
+      image     = "${aws_ecr_repository.ark_repo.repository_url}:${var.ecr_image_tag}"
       essential = true
-
       portMappings = [
         {
           containerPort = var.container_port
-          hostPort      = var.container_port
           protocol      = "tcp"
         }
       ]
@@ -64,10 +67,7 @@ resource "aws_ecs_task_definition" "ark_task" {
   ])
 }
 
-resource "aws_ecs_cluster" "ark_cluster" {
-  name = var.cluster_name
-}
-
+##### ECS Service ###############################################
 resource "aws_ecs_service" "ark_service" {
   name            = var.service_name
   cluster         = aws_ecs_cluster.ark_cluster.id
@@ -80,15 +80,21 @@ resource "aws_ecs_service" "ark_service" {
       aws_subnet.private_a.id,
       aws_subnet.private_b.id
     ]
-    security_groups  = [aws_security_group.alb.id]
+    security_groups  = var.security_group_ids
     assign_public_ip = false
   }
 
   load_balancer {
     target_group_arn = aws_lb_target_group.app_tg.arn
-    container_name   = "ark"
+    container_name   = var.container_name
     container_port   = var.container_port
   }
 
-  depends_on = [aws_iam_role_policy_attachment.ecs_task_execution_policy, aws_lb_target_group.app_tg]
+  depends_on = [
+    aws_iam_role_policy_attachment.ecs_task_execution_policy,
+    aws_lb_target_group.app_tg,
+    aws_subnet.private_a,
+    aws_subnet.private_b,
+    aws_ecs_task_definition.ark_task
+  ]
 }
